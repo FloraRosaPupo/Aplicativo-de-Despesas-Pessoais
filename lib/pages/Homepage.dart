5657d0f089f4b87a8dc163380ea3cff302c26434
FloraRosaPupo <florapupo@gmail.com>
2024-05-06 20:11:48 -0300

    painel inicial

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,3 +1,4 @@
+import 'package:despesaspessoais/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,17 +9,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _transaction = [
+    Transaction(id: 't1', title: 'title', value: 310.8, date: DateTime.now()),
+    Transaction(id: 't2', title: 'title', value: 310.8, date: DateTime.now()),
+    Transaction(id: 't3', title: 'title', value: 310.8, date: DateTime.now()),
+    Transaction(id: 't3', title: 'title', value: 310.8, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Despesas Pessoais",style: TextStyle(color: Colors.white),),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-      ),
-      body: Container(
-        child: Text('Hello World'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(
+            "Despesas Pessoais",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text("Grafico"),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              //map
+              children: _transaction.map((tr) {
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(tr.value.toString()),
+                    ),
+                    Column(
+                      children: [
+                        Text(tr.title),
+                        Text(
+                          tr.date.toString(),
+                        )
+                      ],
+                    )
+                  ],
+                ));
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
